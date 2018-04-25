023087d5082117285237388ff508207af781942f
niuzhihuan <zhihuan.niu@funi.com>
2018-04-25 18:01:40 +0800

    3

---
+++ src/ch03/Java的基本程序设计结构.java
@@ -0,0 +1,59 @@
+package ch03;
+
+/**
+ * 
+ * @author niuzhihuan
+ */
+public class Java的基本程序设计结构 {
+	/**
+	 * 类可以看作一个加载程序逻辑的容器，程序逻辑定义了应用程序的行为。
+	 * 作者通过一个简单的类讲解了程序结构和注意事项。
+	 */
+	public void 一个简单的Java应用程序() {
+	}
+	/**
+	 * 作者介绍了Java三种注释方式
+	 */
+	public void 注释() {
+	}
+	/**
+	 * 4种整型：int、short,long,byte
+	 * 2种浮点型：float,double
+	 * 1种字符类型char
+	 * 1种boolean类型
+	 */
+	public void 数据类型() {
+	}
+	/**
+	 * 变量定义，初始化
+	 * 常量 关键字final 表示这个变量只能被赋值一次。
+	 */
+	public void 变量() {
+	}
+	/**
+	 * 数学函数与常量
+	 * 数值类型之间的转换
+	 * 强制类型转换
+	 * 组合赋值与运算符
+	 * 自增和自减运算符
+	 * 关系和boolean运算符
+	 * 位运算符
+	 * 括号与运算符优先级
+	 * 枚举类型
+	 */
+	public void 运算符() {
+	}
+	/**
+	 * 
+	 */
+	public void 字符串() {
+	}
+	public void 输入输出() {
+	}
+	public void 控制流() {
+	}
+	public void 大数值() {
+	}
+	public void 数组() {
+	}
+}
