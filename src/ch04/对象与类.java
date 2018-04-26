961c5398c60a723cefb2c72d168a8a9cc6961f40
niuzhihuan <zhihuan.niu@funi.com>
2018-04-26 17:51:18 +0800

    4

---
+++ src/ch04/对象与类.java
@@ -0,0 +1,60 @@
+package ch04;
+
+/**
+ * 本章主要讲述 面向对象程序设计与编写
+ * @author niuzhihuan
+ */
+public class 对象与类 {
+	/**
+	 * 类
+	 * 对象
+	 * 识别类
+	 * 类之间的关系
+	 */
+	public void 面向对象程序设计概述() {
+	}
+	/**
+	 * 对象与对象变量
+	 * Java类库中的LocalDate类
+	 * 更改器方法与访问器方法
+	 */
+	public void 使用预定义类() {
+	}
+	/**
+	 * Employee类
+	 * 多个源文件的使用
+	 * 剖析Employee类
+	 * 从构造器开始
+	 * 隐式参数与显式参数
+	 * 封装的优点
+	 * 基于类的访问权限
+	 * 私有方法
+	 * final实例域
+	 */
+	public void 用户自定义类() {
+	}
+	/**
+	 * 静态域
+	 * 静态常量
+	 * 静态方法
+	 * 工厂方法
+	 * main方法
+	 */
+	public void 静态域与静态方法() {
+	}
+	/**
+	 * 
+	 */
+	public void 方法参数() {
+	}
+	public void 对象构造() {
+	}
+	public void 包() {
+	}
+	public void 类路径() {
+	}
+	public void 文档注释() {
+	}
+	public void 类设计技巧() {
+	}
+}
