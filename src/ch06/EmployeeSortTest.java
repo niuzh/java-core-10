ac1562263d026d92437c1c0f589177b6601a6efa
niuzhihuan <zhihuan.niu@funi.com>
2018-04-28 17:43:07 +0800

    6

---
+++ src/ch06/EmployeeSortTest.java
@@ -0,0 +1,31 @@
+package ch06;
+
+import java.util.Arrays;
+
+
+/**
+ * 
+ * @author niuzhihuan
+ */
+public class EmployeeSortTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Employee[] staff=new Employee[3];
+		staff[0]=new Employee("carl crecker", 7500);
+		staff[1]=new Employee("Harry hacker", 5000);
+		staff[2]=new Employee("Tony Tester", 4000);
+		
+		for (Employee employee : staff) {
+			employee.raiseSalary(5);
+		}
+		Arrays.sort(staff);
+		for (Employee employee : staff) {
+			System.out.println("name="+employee.getName()+",salary="+employee.getSalary()+",hireDay="+employee.getHireDay());
+		}
+
+	}
+
+}
