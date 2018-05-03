c299e1f4f6ce55a3028e285153323c51d4a341ca
niuzhihuan <zhihuan.niu@funi.com>
2018-05-03 17:44:31 +0800

    9

---
+++ src/ch09/PriorityQueueTest.java
@@ -0,0 +1,30 @@
+package ch09;
+
+import java.util.*;
+import java.time.*;
+
+/**
+ * This program demonstrates the use of a priority queue.
+ * 使用优先级队列的典型示例是任务调度
+ * @version 1.02 2015-06-20
+ * @author Cay Horstmann
+ */
+public class PriorityQueueTest
+{
+   public static void main(String[] args)
+   {
+      PriorityQueue<LocalDate> pq = new PriorityQueue<>();
+      pq.add(LocalDate.of(1916, 12, 9)); // G. Hopper
+      pq.add(LocalDate.of(1906, 12, 9)); // G. Hopper
+      pq.add(LocalDate.of(1815, 12, 10)); // A. Lovelace
+      pq.add(LocalDate.of(1903, 12, 3)); // J. von Neumann
+      pq.add(LocalDate.of(1910, 6, 22)); // K. Zuse
+
+      System.out.println("Iterating over elements...");
+      for (LocalDate date : pq)
+         System.out.println(date);
+      System.out.println("Removing elements...");
+      while (!pq.isEmpty())
+         System.out.println(pq.remove());
+   }
+}
