2dba5101b84a922f7b2c20406fb5928ed3502252
niuzhihuan <zhihuan.niu@funi.com>
2018-05-07 21:42:59 +0800

    13

--- src/ch13/package-info.java
+++ src/ch13/package-info.java
@@ -10,9 +10,13 @@
  * 应用首选项的存储
  * 	属性映射
  * 	首选项API:中心存储库
- * 服务加载器
+ * 服务加载器:插件体系的应用。
  * applet
+ * 	一个简单的applet
+ * 	applet HTML标记和属性
  * Java Web Start
+ * 	分布Java Web Start应用
+ * 	JNLP API
  * @author niuzhihuan
  */
 package ch13;
